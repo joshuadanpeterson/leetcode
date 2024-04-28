a914685d56a9ee93ccfc53a82c14ea613c278624
Josh Peterson <joshuadanpeterson@gmail.com>
2024-04-27 23:35:23 -0600

    Time: 86 ms (86.48%) | Memory: 16.5 MB (63.78%) - LeetSync

--- neetcode-150/arrays/medium/36-valid-sudoku/valid-sudoku.py
+++ neetcode-150/arrays/medium/36-valid-sudoku/valid-sudoku.py
@@ -1,21 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
-
+        rows, cols, squares = {}, {}, {}
+        
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                if board[r][c] == '.':
                     continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
+                val = board[r][c]
+                if val in rows.get(r, set()) or val in cols.get(c, set()) or val in squares.get((r // 3, c // 3), set()):
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
-
+                rows.setdefault(r, set()).add(val)
+                cols.setdefault(c, set()).add(val)
+                squares.setdefault((r // 3, c // 3), set()).add(val)
         return True
