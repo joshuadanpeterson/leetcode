3c33e4cf56cff19d7e8665cea583be20064e3273
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-06 00:59:54 -0600

    Time: 90 ms (91.30%) | Memory: 18 MB (29.18%) - LeetSync

--- neetcode-dsa/linked-lists/singly-linked-lists/easy/838-design-linked-list/design-linked-list.py
+++ neetcode-dsa/linked-lists/singly-linked-lists/easy/838-design-linked-list/design-linked-list.py
@@ -1,4 +1,8 @@
-# doubly linked list
+# doubly linked list with hashmap
+# pylint: disable=too-few-public-methods, invalid-name
+# pylint: disable=missing-module-docstring missing-class-docstring, missing-function-docstring, missing-method-docstring
+
+
 class Node:
     def __init__(self, val=0, prev=None, next=None):
         self.val = val
@@ -10,77 +14,73 @@ class MyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-
-    def get(self, index: int) -> int:
-        current = self.head
-        count = 0
-        while current:
-            if count == index:
-                return current.val
-            current = current.next
-            count += 1
-        return -1
+        self.size = 0
 
     def addAtHead(self, val: int) -> None:
         new_node = Node(val, None, self.head)
         if self.head:
             self.head.prev = new_node
         self.head = new_node
-        if not self.tail:
+        if self.tail is None:
             self.tail = new_node
+        self.size += 1
 
     def addAtTail(self, val: int) -> None:
         new_node = Node(val, self.tail, None)
         if self.tail:
             self.tail.next = new_node
         self.tail = new_node
-        if not self.head:
+        if self.head is None:
             self.head = new_node
+        self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
         if index == 0:
             self.addAtHead(val)
             return
-        if self.tail:
-            node = self.head
-            for _ in range(index - 1):
-                if node is None:
-                    return
-                node = node.next
-            if node is self.tail:
-                self.addAtTail(val)
-            elif node and node.next:
-                new_node = Node(val, node, node.next)
-                node.next.prev = new_node
-                node.next = new_node
-
-    def deleteAtIndex(self, index: int) -> None:
-        if index == 0 and self.head:
-            if self.head == self.tail:
-                self.head = self.tail = None
-            else:
-                self.head = self.head.next
-                self.head.prev = None
+        if index == self.size:
+            self.addAtTail(val)
             return
         node = self.head
         for _ in range(index):
-            if node is None:
-                return
             node = node.next
+        new_node = Node(val, node.prev, node)
+        node.prev.next = new_node
+        node.prev = new_node
+        self.size += 1
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        node = self.head
+        for _ in range(index):
+            node = node.next
+        return node.val
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        node = self.head
+        for _ in range(index):
+            node = node.next
+        if node.prev:
+            node.prev.next = node.next
+        if node.next:
+            node.next.prev = node.prev
+        if node == self.head:
+            self.head = node.next
         if node == self.tail:
-            self.tail = self.tail.prev
-            self.tail.next = None
-        elif node:
-            if node.prev:
-                node.prev.next = node.next
-            if node.next:
-                node.next.prev = node.prev
+            self.tail = node.prev
+        self.size -= 1
+
 
 # Example usage
-# obj = MyDoublyLinkedList()
-# obj.addAtHead(1)
-# obj.addAtTail(3)
-# obj.addAtIndex(1, 2)
-# print(obj.get(1))  # Should return 2
-# obj.deleteAtIndex(1)
-# print(obj.get(1))  # Should return 3
+obj = MyLinkedList()
+obj.addAtHead(1)
+obj.addAtTail(3)
+obj.addAtIndex(1, 2)
+print(obj.get(1))  # Output: 2
+obj.deleteAtIndex(1)
+print(obj.get(1))  # Output: 3
