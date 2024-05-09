25b6ff8746ce9e253b92514463d0ac8139c77067
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-09 00:33:33 -0600

    Time: 37 ms (64.91%) | Memory: 16.4 MB (95.66%) - LeetSync

--- neetcode-dsa/linked-lists/singly-linked-lists/easy/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ neetcode-dsa/linked-lists/singly-linked-lists/easy/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,33 +1,10 @@
-from typing import Optional
-
-
-class ListNode:
-    """Class to represent a node in a singly linked list."""
-
-    def __init__(self, val=0, next_node=None):
-        self.val = val
-        self.next = next_node
-
-
+# Recursive
 class Solution:
-    """Class to merge two sorted linked lists into one sorted list."""
-
-    def mergeTwoLists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        """Merge two sorted linked lists and return it as a new sorted list."""
-        dummy = ListNode()
-        tail = dummy
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-
-        tail.next = list1 or list2
-
-        return dummy.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+        lil.next = self.mergeTwoLists(lil.next, big)
+        return lil
